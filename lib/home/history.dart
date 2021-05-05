1d7edba997abdf3e392bf5c2695e749a35ae9fc6
DananjayaBC <Dana.chanuka@gmail.com>
2021-05-05 11:25:39 +0530

    update 3.0

--- lib/home/history.dart
+++ lib/home/history.dart
@@ -1,21 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:animalbook/widgets/provider_widet.dart';
 
-class History extends StatefulWidget {
-  History({Key key}) : super(key: key);
+class History extends StatelessWidget {
+  const History({Key key}) : super(key: key);
 
-  @override
-  _HistoryState createState() => _HistoryState();
-}
-
-class _HistoryState extends State<History> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(245, 48, 111, 1.0),
         title: Center(
-          child: Text('Vehicle Service'),
+            child: Text(
+          'Tickets',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        )),
+      ),
+      body: StreamBuilder(
+          stream: getUsersTripsStreamSnapshots(context),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text("Loading...");
+            return new ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    buildTripCard(context, snapshot.data.documents[index]));
+          }),
+    );
+  }
+
+  Stream<QuerySnapshot> getUsersTripsStreamSnapshots(
+      BuildContext context) async* {
+    final uid = await Provider.of(context).auth.getCurrentUID();
+    yield* Firestore.instance
+        .collection('veterinarian')
+        .document(uid)
+        .collection('Bookings')
+        .orderBy('date', descending: true)
+        .snapshots();
+  }
+
+  Widget buildTripCard(BuildContext context, DocumentSnapshot payment) {
+    return new GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                  child: Row(children: <Widget>[
+                    Text(
+                      payment['petName'],
+                      style: new TextStyle(fontSize: 25.0),
+                    ),
+                  ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Doctor - ${(payment['doctor'])} ",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+                  child: Row(children: <Widget>[
+                    Text(
+                      "Time - ${(payment['selectTime'])} ",
+                      style: TextStyle(color: Colors.blue, fontSize: 20.00),
+                    ),
+                  ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Pet Type - ${(payment['petType'])} ",
+                        style: new TextStyle(fontSize: 35.0),
+                      ),
+                      Spacer(),
+                      Icon(Icons.train_rounded),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        backgroundColor: Colors.red,
       ),
     );
   }
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:animalbook/widgets/provider_widet.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -8,14 +11,194 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController value1 = new TextEditingController();
+  TextEditingController value2 = new TextEditingController();
+  TextEditingController value3 = new TextEditingController();
+  TextEditingController value4 = new TextEditingController();
+  String selectedUser;
+  String selectTime;
+  String selectType;
+  String formattedDate1;
+
   @override
   Widget build(BuildContext context) {
+    var now = new DateTime.now();
+    var date = new DateFormat('yyyy-MM-dd');
+
+    formattedDate1 = date.format(now);
+    print(formattedDate1);
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text('Pakayas'),
+          child: Text('Veterinarian Booking'),
         ),
         backgroundColor: Colors.red,
+      ),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                  padding: EdgeInsets.all(32),
+                  child: Form(
+                    child: Column(children: [
+                      new DropdownButton<String>(
+                          hint: Text('Select Doctor'),
+                          dropdownColor: Colors.grey,
+                          icon: Icon(Icons.arrow_drop_down),
+                          iconSize: 36,
+                          isExpanded: true,
+                          items: <String>[
+                            'Dr.Dimithra',
+                            'Dr.Dimithra 2',
+                            'Dr.Dimithra 3',
+                            'Dr.Dimithra 4'
+                          ].map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
+                              child: new Text(
+                                value,
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18),
+                              ),
+                            );
+                          }).toList(),
+                          value: selectedUser,
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectedUser = newValue;
+                            });
+                          }),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      new DropdownButton<String>(
+                          hint: Text('Select Time'),
+                          dropdownColor: Colors.grey,
+                          icon: Icon(Icons.arrow_drop_down),
+                          iconSize: 36,
+                          isExpanded: true,
+                          items: <String>[
+                            '4pm - 5pm',
+                            '5pm - 6pm',
+                            '6pm - 7pm',
+                            '7pm - 8pm'
+                          ].map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
+                              child: new Text(
+                                value,
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18),
+                              ),
+                            );
+                          }).toList(),
+                          value: selectTime,
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectTime = newValue;
+                            });
+                          }),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      new DropdownButton<String>(
+                          hint: Text('Pet Type'),
+                          dropdownColor: Colors.grey,
+                          icon: Icon(Icons.arrow_drop_down),
+                          iconSize: 36,
+                          isExpanded: true,
+                          items: <String>['Dog', 'Cat', 'Bird', 'Rabit']
+                              .map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
+                              child: new Text(
+                                value,
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18),
+                              ),
+                            );
+                          }).toList(),
+                          value: selectType,
+                          onChanged: (newValue) {
+                            setState(() {
+                              selectType = newValue;
+                            });
+                          }),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextField(
+                        controller: value1,
+                        decoration: InputDecoration(
+                          labelText: "Pet Name",
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      TextField(
+                        controller: value2,
+                        decoration: InputDecoration(
+                          labelText: "Note",
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: RawMaterialButton(
+                          onPressed: () async {
+                            final uid =
+                                await Provider.of(context).auth.getCurrentUID();
+                            final name = await Provider.of(context)
+                                .auth
+                                .getCurrentUser();
+
+                            await Firestore.instance
+                                .collection("veterinarian")
+                                .document(uid)
+                                .collection('Bookings')
+                                .add({
+                              'doctor': selectedUser,
+                              'selectTime': selectTime,
+                              'petType': selectType,
+                              'petName': value1.text,
+                              'note': value2.text,
+                              'user': name.displayName,
+                              'date': formattedDate1,
+                            });
+                          },
+                          fillColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              "Book",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
